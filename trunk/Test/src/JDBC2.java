01d4922275cb739b0064a4ccb8f727af6f6147db
johnsonreyliu@gmail.com <johnsonreyliu@gmail.com@11a1df18-b2d5-f3e2-f7ce-6eeec1087b1b>
2012-04-07 22:05:34 +0000

    adding jdbc2 and 3

---
+++ trunk/Test/src/JDBC2.java
@@ -0,0 +1,23 @@
+//JDBC Example - deleting a record
+// Coded by Checn Li/Kirill Petrov Winter, 2005
+// Slightly revised for ICS185 Spring 2005, by Norman Jacobson
+	
+
+import java.sql.*;              // Enable SQL processing
+
+public class JDBC2
+{
+    public static void main(String[] arg) throws Exception
+    {
+        // Incorporate mySQL driver
+        Class.forName("com.mysql.jdbc.Driver").newInstance();
+
+        // Connect to the test database
+        Connection connection = DriverManager.getConnection("jdbc:mysql:///moviedb","mytestuser", "mypassword");
+
+        // create update DB statement -- deleting second record of table; return status
+        Statement update = connection.createStatement();
+        int retID = update.executeUpdate("delete from stars where id = 755011");
+        System.out.println("retID = " + retID);
+    }
+}
---
+++ trunk/Test/src/JDBC3.java
@@ -0,0 +1,34 @@
+// JDBC Example - updating a DB via SQL template and value groups
+// Coded by Chen Li/Kirill Petrov Winter, 2005
+// Slightly revised for ICS185 Spring 2005, by Norman Jacobson
+
+import java.sql.*;                              // Enable SQL processing
+
+public class JDBC3
+{
+    public static void main(String[] arg) throws Exception
+    {
+	        // Incorporate MySQL driver
+	         Class.forName("com.mysql.jdbc.Driver").newInstance();
+	
+	        // Connect to the test database
+	        Connection connection =
+	            DriverManager.getConnection("jdbc:mysql:///moviedb", "mytestuser", "mypassword");
+	
+	        // prepare SQL statement template that's to be repeatedly excuted
+	        String updateString = "update stars set first_name = ? where id = ?";
+	        PreparedStatement updateStars = connection.prepareStatement(updateString);
+	
+	        // values for first and second "?" wildcard in statement template
+	        int [] ids = {755011, 755017};
+	        String [] firstNames = {"New Arnold", "New Eddie"};
+	
+	        // for each record in table, update to new values given
+	        for(int i = 0; i < ids.length; i++)
+	            {
+	                updateStars.setString(1, firstNames[i]);
+	                updateStars.setInt(2, ids[i]);
+	                updateStars.executeUpdate();
+	            }
+    }
+}
